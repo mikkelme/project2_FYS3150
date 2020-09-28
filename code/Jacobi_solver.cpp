19880f3e348fba9cbfca46e19c59e75e9d2f26d8
Mikkel Metzsch Jensen <mikkelme@mbptilhdemikkel.intern>
2020-09-28 07:36:25 +0200

    mutiple solvers

--- code/Jacobi_solver.cpp
+++ code/Jacobi_solver.cpp
@@ -33,24 +33,35 @@ int main(int argc, char const *argv[]) {
   double maxnondig;
   int p; int q;
 
+  bool jacobi_solve = True
+  bool armadillo_solve = False
+
   clock_t start, finish;
   start = clock();
 
-  my_functions.MaxOffdiag(A, p, q, maxnondig, n);
-  while (maxnondig > tol && iter <= maxiter){
-    my_functions.JacobiRotate(A, R, p, q, n);
+  if (Jacobi_solve == True){
     my_functions.MaxOffdiag(A, p, q, maxnondig, n);
-    iter++;
+    while (maxnondig > tol && iter <= maxiter){
+      my_functions.JacobiRotate(A, R, p, q, n);
+      my_functions.MaxOffdiag(A, p, q, maxnondig, n);
+      iter++;
+      }
+    finish = clock();
+    double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
+    mat Eigval = my_functions.OrderEigenResults(A, R, n);
+  }
+  if (armadillo_solve == True){
+    vec Eigval(N);
+    eig_sym(Eigval, A);
+    finish = clock();
+    double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
   }
 
-  finish = clock();
-  double timeused = (double) (finish - start)/(CLOCKS_PER_SEC );
-
-  //Order result and write to files
-  mat Eigval = my_functions.OrderEigenResults(A, R, n);
+  //Write results
   my_functions.WriteIter(iter - 1);
   my_functions.WriteMeanError(Eigval, d, a, n);
   my_functions.WriteTime(timeused);
+
 
 
 return 0;
